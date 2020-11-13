7a2e2f106616185db664b467cd220b4166df50db
Claudia <claudia@bliinx.ca>
2020-11-12 23:22:40 -0500

    manhathan heuristic

--- heuristics.py
+++ heuristics.py
@@ -2,7 +2,8 @@ import numpy as np
 from Puzzle import *
 from constants import *
 
-''' naive heuristic for demo'''
+
+# naive heuristic for demo
 def h0_naive(puzzle):
     r, c = puzzle.state.shape
     if puzzle.state[r - 1, c - 1] == 0:
@@ -10,10 +11,30 @@ def h0_naive(puzzle):
     else:
         return 1
 
-''' hamming loss'''
+
+# hamming loss
 def h1_hamming(puzzle):
     if puzzle.goal_state == []:
         puzzle.goal_state = goal_state1
+
     config = puzzle.state
     goal = puzzle.goal_state
     return np.count_nonzero(np.bitwise_xor(config, goal))
+
+
+# Manhattan loss
+def h2_manhathan(puzzle_array):
+    puzzle = Puzzle(puzzle_array)
+
+    if not puzzle.goal_state:
+        puzzle.goal_state = goal_state1
+
+    config = puzzle.state
+    goal_arr = puzzle.goal_state
+    state = config.flatten().tolist()
+    goal = goal_arr.flatten().tolist()
+
+    total = sum(abs(a % 4 - b % 4) + abs(a // 4 - b // 4)
+                for a, b in ((state.index(i), goal.index(i)) for i in range(0, 8)))
+
+    return total
