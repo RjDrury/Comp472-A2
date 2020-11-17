1c29d42d8f26fd16bce536db4a767841db0f966b
Rob <rdrury1513@gmail.com>
2020-11-17 14:17:11 -0500

    added 50 random puzzles

---
+++ generate_50_puzzles.py
@@ -0,0 +1,14 @@
+import random
+def generate_puzzles():
+    output_file = open("random_puzzles.txt", "w")
+    for i in range (50):
+        arr = [0,1,2,3,4,5,6,7,8,9]
+        random.shuffle(arr)
+        
+
+        for item in arr:
+            output_file.write(str(item) + " ")
+        output_file.write('\n')
+
+if __name__ == '__main__':
+    generate_puzzles()
